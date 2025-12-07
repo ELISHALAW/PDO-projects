55def63de2fed39bc6a96a645c6859836db445c5
ELISHALAW <seongchunlaw050@gmail.com>
2025-12-07 22:01:39 +0800

    update

--- foot.php
+++ foot.php
@@ -1,6 +1,7 @@
 <section class="footer">
         <div class="credit">Created by <span>Law Seong Chun</span> | All rights reserved</div>
     </section>
+    <h1>shm_detach</h1>
     <!-- Footer section ends -->
 </body>
 <script src="/js/app.js"></script>
